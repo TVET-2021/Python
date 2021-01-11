e6192e793e5fe92095babd7b736c80abdb010316
tvet2021 <77226659+tvet2021@users.noreply.github.com>
2021-01-11 09:36:17 +0900

    Add files via upload

---
+++ Lab/Lab02.py
@@ -0,0 +1,19 @@
+
+# ##################################################################
+# Lab 2
+# - Print your information
+# ##################################################################
+
+name = "ratataca"
+home_address = "cheonan, republic of korea"
+organizaton = "koreatech"
+
+# 1. One line
+print(name + " " + home_address + " " + organizaton)
+print("{} {} {}".format(name, home_address, organizaton))
+print(f"{name} {home_address} {organizaton}")
+
+# 2. Multi line
+print(name + "\n" + home_address + "\n" + organizaton)
+print("{}\n{}\n{}".format(name, home_address, organizaton))
+print(f"{name}\n{home_address} \n{organizaton}")
